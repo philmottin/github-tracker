0d905d9439010d1b9ff7d0f0c3fc0007e92d1cb7
Phil Mottin <philmottin@gmail.com>
2021-02-17 00:04:38 +0000

    Fetch data with axios
    Add axios package
    In App.js Fetch users from github and pass as props

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ loading: false, users: res.data });
+    console.log(res.data);
+
+    //axios
+    //  .get('https://api.github.com/users')
+    //  .then((res) => console.log(res.data));
+  }
+
   render() {
     return (
       <div>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
