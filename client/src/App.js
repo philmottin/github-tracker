0a1993fa0dab6d677ddf876294720a96f7c8fd70
Phil Mottin <philmottin@gmail.com>
2021-02-17 04:08:56 +0000

    Add Search Component and clear button
    Cleanup and destructure in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+
 import axios from 'axios';
 import './App.css';
 
@@ -10,29 +12,32 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+  searchUsers = async (text) => {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ loading: false, users: res.data });
-    //console.log(res.data);
+    this.setState({ loading: false, users: res.data.items });
+  };
 
-    //axios
-    //  .get('https://api.github.com/users')
-    //  .then((res) => console.log(res.data));
-  }
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div>
         <Navbar />
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
