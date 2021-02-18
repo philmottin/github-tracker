4bc41814bc7a194107c255e8f9e0ebe7eb852f6c
Phil Mottin <philmottin@gmail.com>
2021-02-18 01:41:30 +0000

    Refactor searchUsers method to Context API
    Added githubReducers

--- client/src/components/users/Search.jsx
+++ client/src/components/users/Search.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+const Search = ({ showClear, clearUsers, setAlert }) => {
+  const githubContext = useContext(GithubContext);
+
   const [text, setText] = useState('');
 
   //this.setState({ text: e.target.value });
@@ -12,7 +15,7 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
     if (text === '') {
       setAlert('Please enter something', 'light');
     } else {
-      searchUsers(text);
+      githubContext.searchUsers(text);
       setText('');
     }
   };
@@ -43,7 +46,6 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
 };
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   setAlert: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
